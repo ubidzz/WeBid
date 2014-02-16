38f6c1b6210a633187f2d306cb4fa5f8fc855c6d
Jason <ubidzz@live.com>
2014-02-15 21:54:20 -0500

    Add the login with facebook mod
    Add the login with facebook mod to usersettings.php page this will give you control of the login with facebook where you can add your app id, app secret and turn the login with facebook mod on or off.
    
    Renlok wanted me to add the login with facebook mod to webid.

--- admin/usersettings.php
+++ admin/usersettings.php
@@ -26,16 +26,28 @@ if (isset($_POST['action']) && $_POST['action'] == 'update')
 	// Update database
 	$query = "UPDATE ". $DBPrefix . "settings SET
 			  usersauth = '" . $_POST['usersauth'] . "',
-			  activationtype = " . intval($_POST['usersconf']) . "";
+			  activationtype = " . intval($_POST['usersconf']) . ",
+			  facebook_login = '" . $_POST['facebook_login'] . "',
+			  facebook_app_id = '" . $_POST['facebook_app_id'] . "',
+			  facebook_app_secret = '" . $_POST['facebook_app_secret'] . "'";
 	$system->check_mysql(mysql_query($query), $query, __LINE__, __FILE__);
 	$ERR = $MSG['895'];
 
 	$system->SETTINGS['usersauth'] = $_POST['usersauth'];
 	$system->SETTINGS['activationtype'] = $_POST['usersconf'];
+	$system->SETTINGS['facebook_login'] = $_POST['facebook_login'];
+	$system->SETTINGS['facebook_app_id'] = $_POST['facebook_app_id'];
+	$system->SETTINGS['facebook_app_secret'] = $_POST['facebook_app_secret'];
 }
 
 loadblock($MSG['25_0151'], $MSG['25_0152'], 'yesnostacked', 'usersauth', $system->SETTINGS['usersauth'], array($MSG['2__0066'], $MSG['2__0067']));
 loadblock($MSG['25_0151_a'], $MSG['25_0152_a'], 'select3num', 'usersconf', $system->SETTINGS['activationtype'], array($MSG['25_0152_b'], $MSG['25_0152_c'], $MSG['25_0152_d']));
+
+////connect with facebook
+loadblock($MSG['350_10201'], $MSG['350_10200'], '', '', array(), true);
+loadblock($MSG['350_10196'], $MSG['350_10197'], 'yesnostacked', 'facebook_login', $system->SETTINGS['facebook_login'], array($MSG['030'], $MSG['029']));
+loadblock($MSG['350_10194'], '', 'text', 'facebook_app_id', $system->SETTINGS['facebook_app_id']);
+loadblock($MSG['350_10195'], '', 'text', 'facebook_app_secret', $system->SETTINGS['facebook_app_secret']);
 
 $template->assign_vars(array(
 		'ERROR' => (isset($ERR)) ? $ERR : '',
